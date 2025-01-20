b2a75e948f826d42c7a5a62cbdebb8fef4c1ed58
민수아 <Minsua0503@gmail.com>
2025-01-20 21:45:07 +0900

    chore :: @EnableConfigurationProperties 추가

--- src/main/java/com/example/springstudy/SpringStudyApplication.java
+++ src/main/java/com/example/springstudy/SpringStudyApplication.java
@@ -1,9 +1,12 @@
 package com.example.springstudy;
 
+import com.example.springstudy.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
