cf55cb7f7515bca473cce980eea4b059e8dea60c
민수아 <Minsua0503@gmail.com>
2025-01-20 18:11:23 +0900

    feat :: User 엔티티 추가

---
+++ src/main/java/com/example/springstudy/domain/user/domain/User.java
@@ -0,0 +1,23 @@
+package com.example.springstudy.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String password;
+
+}
