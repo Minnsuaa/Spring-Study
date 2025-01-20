705afecf59b2937285a87246bc45e4c7969ef878
민수아 <Minsua0503@gmail.com>
2025-01-20 21:46:08 +0900

    feat :: 예외 추가

--- src/main/java/com/example/springstudy/global/error/ErrorCode.java
+++ src/main/java/com/example/springstudy/global/error/ErrorCode.java
@@ -7,7 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+
+    // user
+    USER_NOT_FOUND(404, "User Not Found"),
+
+    // jwt
+    EXPIRED_JWT(401, "Expired JWT"),
+    INVALID_JWT(401, "Invalid JWT"),
+    INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token");
 
     private final int status;
     private final String message;
