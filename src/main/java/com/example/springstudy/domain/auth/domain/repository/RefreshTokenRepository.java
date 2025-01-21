7b3d799c890edf3d4c01f4abc173644d94f3d1f5
민수아 <Minsua0503@gmail.com>
2025-01-21 22:36:02 +0900

    feat ::  findByRefreshToken 쿼리 추가

--- src/main/java/com/example/springstudy/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/springstudy/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,10 @@ package com.example.springstudy.domain.auth.domain.repository;
 import com.example.springstudy.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
 }
