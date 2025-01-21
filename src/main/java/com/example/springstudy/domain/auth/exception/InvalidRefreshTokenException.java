258270fd4c5f82c7be09badcc9261ea367baf313
민수아 <Minsua0503@gmail.com>
2025-01-21 22:40:48 +0900

    feat :: RefreshToken 예외 처리 추가

---
+++ src/main/java/com/example/springstudy/domain/auth/exception/InvalidRefreshTokenException.java
@@ -0,0 +1,14 @@
+package com.example.springstudy.domain.auth.exception;
+
+import com.example.springstudy.global.error.CustomException;
+import com.example.springstudy.global.error.ErrorCode;
+
+public class InvalidRefreshTokenException extends CustomException {
+
+    public static final CustomException EXCEPTION = new InvalidRefreshTokenException();
+
+    private InvalidRefreshTokenException() {
+        super(ErrorCode.INVALID_REFRESH_TOKEN);
+    }
+
+}
---
+++ src/main/java/com/example/springstudy/domain/auth/exception/RefreshTokenNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.springstudy.domain.auth.exception;
+
+import com.example.springstudy.global.error.CustomException;
+import com.example.springstudy.global.error.ErrorCode;
+
+public class RefreshTokenNotFoundException extends CustomException {
+
+    public static final CustomException EXCEPTION = new RefreshTokenNotFoundException();
+
+    private RefreshTokenNotFoundException() {
+        super(ErrorCode.REFRESH_TOKEN_NOT_FOUND);
+    }
+
+}
