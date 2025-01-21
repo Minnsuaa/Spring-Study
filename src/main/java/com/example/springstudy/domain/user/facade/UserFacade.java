4abf11293ab6334bcba01988fe45f09e0c019538
민수아 <Minsua0503@gmail.com>
2025-01-21 22:39:03 +0900

    feat :: UserFacade 추가

---
+++ src/main/java/com/example/springstudy/domain/user/facade/UserFacade.java
@@ -0,0 +1,27 @@
+package com.example.springstudy.domain.user.facade;
+
+import com.example.springstudy.domain.user.domain.User;
+import com.example.springstudy.domain.user.domain.repository.UserRepository;
+import com.example.springstudy.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
