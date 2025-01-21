f7f7276d4da57301e0adb4efc73f73ba8d8c2f90
민수아 <Minsua0503@gmail.com>
2025-01-21 22:38:48 +0900

    feat :: 회원가입 서비스 추가

---
+++ src/main/java/com/example/springstudy/domain/auth/service/SignupService.java
@@ -0,0 +1,30 @@
+package com.example.springstudy.domain.auth.service;
+
+import com.example.springstudy.domain.auth.presentation.dto.request.AuthRequest;
+import com.example.springstudy.domain.user.domain.User;
+import com.example.springstudy.domain.user.domain.repository.UserRepository;
+import com.example.springstudy.domain.user.exception.UserExistException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SignupService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(AuthRequest request) {
+        if(userRepository.findByAccountId(request.getAccountId()).isPresent())
+            throw UserExistException.EXCEPTION;
+
+        userRepository.save(User.builder()
+                        .accountId(request.getAccountId())
+                        .password(passwordEncoder.encode(request.getPassword()))
+                .build());
+    }
+
+}
