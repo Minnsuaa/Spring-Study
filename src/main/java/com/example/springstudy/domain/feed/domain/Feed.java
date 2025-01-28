763ba2bffba5cf1e69241c9a68aa9cf37227462d
민수아 <Minsua0503@gmail.com>
2025-01-28 13:06:05 +0900

    feat :: Feed 엔티티 추가

---
+++ src/main/java/com/example/springstudy/domain/feed/domain/Feed.java
@@ -0,0 +1,32 @@
+package com.example.springstudy.domain.feed.domain;
+
+import com.example.springstudy.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_feed")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
