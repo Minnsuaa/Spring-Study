9240f535326a71b1baadb00cb877e81d3d1e0a93
민수아 <Minsua0503@gmail.com>
2025-01-29 12:54:36 +0900

    feat :: CannotModifyFeedException 추가

---
+++ src/main/java/com/example/springstudy/domain/feed/exception/CannotModifyFeedException.java
@@ -0,0 +1,14 @@
+package com.example.springstudy.domain.feed.exception;
+
+import com.example.springstudy.global.error.CustomException;
+import com.example.springstudy.global.error.ErrorCode;
+
+public class CannotModifyFeedException extends CustomException {
+
+    public static final CustomException EXCEPTION = new CannotModifyFeedException();
+
+    private CannotModifyFeedException() {
+        super(ErrorCode.CANNOT_MODIFY_FEED);
+    }
+
+}
