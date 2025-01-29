b7a90e873cecce8e90470eba56cc1cfa518b2205
민수아 <Minsua0503@gmail.com>
2025-01-29 12:54:13 +0900

    feat :: CannotDeleteFeedException 추가

---
+++ src/main/java/com/example/springstudy/domain/feed/exception/CannotDeleteFeedException.java
@@ -0,0 +1,14 @@
+package com.example.springstudy.domain.feed.exception;
+
+import com.example.springstudy.global.error.CustomException;
+import com.example.springstudy.global.error.ErrorCode;
+
+public class CannotDeleteFeedException extends CustomException {
+
+    public static final CustomException EXCEPTION = new CannotDeleteFeedException();
+
+    private CannotDeleteFeedException() {
+        super(ErrorCode.CANNOT_DELETE_FEED);
+    }
+
+}
