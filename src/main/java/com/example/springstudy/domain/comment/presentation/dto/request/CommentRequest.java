b248ab63d722625bd18f4114440b568edea2410e
민수아 <Minsua0503@gmail.com>
2025-02-13 12:19:30 +0900

    refactor :: CommentRequest

--- src/main/java/com/example/springstudy/domain/comment/presentation/dto/request/CommentRequest.java
+++ src/main/java/com/example/springstudy/domain/comment/presentation/dto/request/CommentRequest.java
@@ -1,18 +1,17 @@
 package com.example.springstudy.domain.comment.presentation.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class DeleteCommentRequest {
+public class CommentRequest {
 
-    @NotNull
-    private Long commentId;
-
-    @NotNull
-    private Long feedId;
+    @NotBlank
+    @Size(max = 100)
+    private String comment;
 
 }
