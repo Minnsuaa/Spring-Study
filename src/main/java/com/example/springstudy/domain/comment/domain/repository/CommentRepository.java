21d0859187f67fd1f4374e19da87a293c5fb8b69
민수아 <Minsua0503@gmail.com>
2025-02-13 12:25:13 +0900

    feat :: findByFeedId 메서드 추가

--- src/main/java/com/example/springstudy/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/springstudy/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.example.springstudy.domain.comment.domain.repository;
 import com.example.springstudy.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByFeedId(Long feedId);
 }
