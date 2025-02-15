51c887868cef013b9a7c8b993c12ac67393dd46f
민수아 <Minsua0503@gmail.com>
2025-02-15 13:18:49 +0900

    refactor :: int형 상태코드 HttpStatus로 변경

--- src/main/java/com/example/springstudy/global/error/ErrorCode.java
+++ src/main/java/com/example/springstudy/global/error/ErrorCode.java
@@ -2,35 +2,36 @@ package com.example.springstudy.global.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
 
     // user
-    USER_NOT_FOUND(404, "User Not Found"),
-    USER_EXISTS(409, "User Already Exists"),
-    PASSWORD_MISMATCH(400, "Password Mismatch"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found"),
+    USER_EXISTS(HttpStatus.CONFLICT, "User Already Exists"),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "Password Mismatch"),
 
     // jwt
-    EXPIRED_JWT(401, "Expired JWT"),
-    INVALID_JWT(401, "Invalid JWT"),
-    INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token"),
-    REFRESH_TOKEN_NOT_FOUND(404, "Refresh token Not Found"),
+    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired JWT"),
+    INVALID_JWT(HttpStatus.UNAUTHORIZED, "Invalid JWT"),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Refresh Token"),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Refresh token Not Found"),
 
     // feed
-    FEED_NOT_FOUND(404, "Feed Not Found"),
-    CANNOT_DELETE_FEED(403, "Can't Delete Feed"),
-    CANNOT_MODIFY_FEED(403, "Can't Modify Feed"),
+    FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "Feed Not Found"),
+    CANNOT_DELETE_FEED(HttpStatus.FORBIDDEN, "Can't Delete Feed"),
+    CANNOT_MODIFY_FEED(HttpStatus.FORBIDDEN, "Can't Modify Feed"),
 
     // comment
-    COMMENT_NOT_FOUND(404, "Comment Not Found"),
-    CANNOT_DELETE_COMMENT(403, "Can't Delete Comment"),
-    CANNOT_MODIFY_COMMENT(403, "Can't Modify Comment");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Comment Not Found"),
+    CANNOT_DELETE_COMMENT(HttpStatus.FORBIDDEN, "Can't Delete Comment"),
+    CANNOT_MODIFY_COMMENT(HttpStatus.FORBIDDEN, "Can't Modify Comment");
 
-    private final int status;
+    private final HttpStatus status;
     private final String message;
 
 }
