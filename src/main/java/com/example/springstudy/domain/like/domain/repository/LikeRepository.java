b51dacfa5b5610e8172aeda330acb5e11d58e9d8
민수아 <Minsua0503@gmail.com>
2025-03-08 14:35:00 +0900

    feat :: 좋아요 쿼리 추가

---
+++ src/main/java/com/example/springstudy/domain/like/domain/repository/LikeRepository.java
@@ -0,0 +1,12 @@
+package com.example.springstudy.domain.like.domain.repository;
+
+import com.example.springstudy.domain.feed.domain.Feed;
+import com.example.springstudy.domain.like.domain.Like;
+import com.example.springstudy.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    boolean existsByUserAndFeed(User user, Feed feed);
+
+}
