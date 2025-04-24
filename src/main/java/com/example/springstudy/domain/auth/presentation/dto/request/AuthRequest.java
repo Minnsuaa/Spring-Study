a73abc93efb5af9a9f0098bebd1ef62dd282d1bd
민수아 <Minsua0503@gmail.com>
2025-04-24 18:49:38 +0900

    refactor :: class -> record로 변경

--- src/main/java/com/example/springstudy/domain/auth/presentation/dto/request/AuthRequest.java
+++ src/main/java/com/example/springstudy/domain/auth/presentation/dto/request/AuthRequest.java
@@ -2,23 +2,19 @@ package com.example.springstudy.domain.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AuthRequest {
+public record AuthRequest(
 
     @NotBlank
     @Size(min = 8, max = 20)
-    private String accountId;
+    String accountId,
 
     @NotBlank
     @Size(min = 8, max = 20)
-    private String password;
+    String password,
 
     @Size(min = 3, max = 10)
-    private String userName;
+    String userName
 
+) {
 }
